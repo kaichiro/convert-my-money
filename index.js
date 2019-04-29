b95c4e31cf0ecc9a29d3e900b5469da8ec10f398
kaichiro <kcfg23@gmail.com>
2019-04-29 13:30:07 -0300

    Atualizando index.js

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 const PORT = 3000
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
 
 app.listen(PORT, err => {
     if (err) {
