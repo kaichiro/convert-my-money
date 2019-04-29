fa724ad512a820c55db454bcf14a03eb5205d0bc
kaichiro <kcfg23@gmail.com>
2019-04-29 14:44:15 -0300

    Atualizando referências para o index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const path = require('path')
 
+const convert = require('./lib/convert')
+
 const app = express()
 const PORT = 3000
 
@@ -10,6 +12,21 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/cotacao', (req, res) => {
+    const { cotacao, quantidade } = req.query
+    if (cotacao && quantidade) {
+        const conversao = convert.convert(cotacao, quantidade)
+        res.render('cotacao', {
+            cotacao: convert.toMoney(cotacao),
+            quantidade: convert.toMoney(quantidade),
+            conversao: convert.toMoney(conversao),
+            error: false,
+        })
+    } else {
+        res.render('cotacao', { error: 'Invalid values!' })
+    }
 })
 
 app.listen(PORT, err => {
