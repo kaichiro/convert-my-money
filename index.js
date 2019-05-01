acef550f142619422e6bd89fd2bd84004a944942
kaichiro <kcfg23@gmail.com>
2019-05-01 18:36:21 -0300

    Implementando logica e view para comunicacao API BancoCentralBrasil

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 
 const convert = require('./lib/convert')
+const apiBCB = require('./lib/api.bcb')
 
 const app = express()
 const PORT = 3000
@@ -10,8 +11,11 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
-    res.render('home')
+app.get('/', async (req, res) => {
+    const cotacao = await apiBCB.getCotacao()
+    res.render('home', {
+        cotacao
+    })
 })
 
 app.get('/cotacao', (req, res) => {
