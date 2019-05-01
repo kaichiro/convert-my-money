cb3f6208fb080a12ac73043a3e1ff2550aa4b427
kaichiro <kcfg23@gmail.com>
2019-05-01 18:52:12 -0300

    Criando automatismo para PULL e PUSH

---
+++ pull.bat
@@ -0,0 +1,4 @@
+git checkout dev
+git pull origin dev
+git checkout master
+git pull origin master
---
+++ push.bat
@@ -0,0 +1,5 @@
+git checkout dev
+git push origin dev
+
+git checkout master
+git push origin master
